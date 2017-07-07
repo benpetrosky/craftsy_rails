8c94b5223fa3aa4db8c3fb61ad478910373de073
Ben Petrosky <benpetrosky@hotmail.com>
2017-07-07 09:43:27 -0700

    add user Chuck Petrosky as administrator through rails console, and provide space where admin rights will be applied

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!, except: [:home]
 
-  
+
   def home
 
   end
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :admin])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :admin])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
   end
 end
