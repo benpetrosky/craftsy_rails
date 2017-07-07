bc0a7e2e3c1d9ab0b8a5818d31fc2eda8b110bca
Ben Petrosky <benpetrosky@hotmail.com>
2017-07-07 11:29:48 -0700

    provide specs that will generate a simple cov score of 81.25 percent for all files

--- Gemfile
+++ Gemfile
@@ -41,6 +41,7 @@ group :development, :test do
   gem 'launchy'
   gem 'pry'
   gem 'shoulda-matchers'
+  gem 'simplecov', require: false
 end
 
 group :development do
---
+++ spec/models/review_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe Review, type: :model do
+  it { should validate_presence_of :author }
+  it { should validate_presence_of :comment }
+  it { should belong_to :product }
+end
